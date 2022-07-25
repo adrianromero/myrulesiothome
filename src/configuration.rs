7a63b3e953c9e74994831618a84d0ecd329c74ca
Adrián <adrianromero@users.noreply.github.com>
2022-07-25 18:56:06 +0200

    Initial commit

---
+++ src/configuration.rs
@@ -0,0 +1,97 @@
+//    MyRulesIoT  Project is a rules engine for MQTT based on MyRulesIoT lib
+//    Copyright (C) 2022 Adrián Romero Corchado.
+//
+//    This file is part of MyRulesIoT.
+//
+//    MyRulesIoT is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    MyRulesIoT is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with MyRulesIoT.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+use rumqttc::{AsyncClient, ClientError, EventLoop, QoS};
+use std::collections::HashMap;
+
+use myrulesiot::mqtt;
+use myrulesiot::mqtt::{ActionMessage, ConnectionMessage, ConnectionValues};
+
+use myrulesiot::rules::forward;
+use myrulesiot::rules::lights;
+use myrulesiot::rules::savelist;
+use myrulesiot::rules::zigbee;
+
+pub async fn connect_mqtt() -> Result<(AsyncClient, EventLoop), ClientError> {
+    // Defines connection properties
+    let connection_info = ConnectionValues {
+        id: "rustclient-231483".into(),
+        host: "localhost".into(),
+        clean_session: true,
+        ..Default::default()
+    };
+    let subscriptions = &[
+        ("myhelloiot/#", QoS::AtMostOnce),
+        ("zigbee2mqtt/0x000b57fffe323b4d", QoS::AtMostOnce), // presence sensor
+        ("zigbee2mqtt/0x000b57fffe4fc5ca", QoS::AtMostOnce), // remote control
+        ("SYSMR/system_action", QoS::AtMostOnce),
+        ("ESPURNA04/#", QoS::AtMostOnce),
+    ];
+
+    mqtt::new_connection(connection_info, subscriptions).await
+}
+
+type FnReducer =
+    Box<dyn FnOnce(&mut HashMap<String, Vec<u8>>, &ActionMessage) -> Vec<ConnectionMessage>>;
+
+type ReducersVec = Vec<FnReducer>;
+
+pub fn app_map_reducers() -> ReducersVec {
+    vec![
+        Box::new(savelist::save_value("SYSMR/user_action/tick")),
+        Box::new(forward::forward_user_action_tick("myhelloiot/timer")),
+        // Box::new(rules::light_actions("myhelloiot/light1")),
+        // Box::new(rules::modal_value("myhelloiot/alarm")),
+        Box::new(savelist::save_list(
+            "myhelloiot/temperature",
+            &chrono::Duration::seconds(20),
+            40,
+        )),
+        // Box::new(forward::forward_action(
+        //     "zigbee2mqtt/0x000b57fffe4fc5ca",
+        //     "ESPURNA04/relay/0/set",
+        // )),
+        // Box::new(lights::toggle(
+        //     zigbee::actuator_toggle("zigbee2mqtt/0x000b57fffe4fc5ca"),
+        //     "ESPURNA04/relay/0",
+        //     "ESPURNA04/relay/0/set",
+        // )),
+        Box::new(lights::light_time(
+            zigbee::actuator_toggle("zigbee2mqtt/0x000b57fffe4fc5ca"),
+            "ESPURNA04/relay/0",
+            "ESPURNA04/relay/0/set",
+        )),
+        Box::new(lights::light_on(
+            zigbee::actuator_brightness_up("zigbee2mqtt/0x000b57fffe4fc5ca"),
+            "ESPURNA04/relay/0",
+            "ESPURNA04/relay/0/set",
+        )),
+        Box::new(lights::light_off(
+            zigbee::actuator_brightness_down("zigbee2mqtt/0x000b57fffe4fc5ca"),
+            "ESPURNA04/relay/0",
+            "ESPURNA04/relay/0/set",
+        )),
+        Box::new(lights::light_time_reset(
+            "ESPURNA04/relay/0",
+            "ESPURNA04/relay/0/set",
+        )),
+        Box::new(lights::status("ESPURNA04/relay/0")),
+        // Box::new(devices::simulate_relay("ESPURNA04/relay/0")),
+    ]
+}
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+//    MyRulesIoT  Project is a rules engine for MQTT based on MyRulesIoT lib
+//    Copyright (C) 2022 Adrián Romero Corchado.
+//
+//    This file is part of MyRulesIoT.
+//
+//    MyRulesIoT is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    MyRulesIoT is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with MyRulesIoT.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+use std::collections::HashMap;
+use std::error::Error;
+
+use tokio::sync::mpsc;
+use tokio::try_join;
+
+use myrulesiot::engine;
+use myrulesiot::mqtt::{self, ActionMessage, ConnectionMessage, ConnectionResult, ConnectionState};
+use myrulesiot::timer;
+
+mod configuration;
+
+fn app_final(_: &HashMap<String, Vec<u8>>, action: &ActionMessage) -> bool {
+    action.matches_action("SYSMR/system_action", "exit".into())
+}
+
+fn app_reducer(state: ConnectionState, action: ActionMessage) -> ConnectionState {
+    let mut messages = Vec::<ConnectionMessage>::new();
+    let mut newmap = state.info.clone();
+
+    for f in configuration::app_map_reducers() {
+        messages.append(&mut f(&mut newmap, &action));
+    }
+
+    let is_final = app_final(&state.info, &action);
+
+    ConnectionState {
+        info: newmap,
+        messages,
+        is_final,
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
+    log::info!("Starting myrulesiot...");
+    let (client, eventloop) = configuration::connect_mqtt().await?;
+
+    let (sub_tx, sub_rx) = mpsc::channel::<ActionMessage>(10);
+    let (pub_tx, pub_rx) = mpsc::channel::<ConnectionResult>(10);
+
+    let timertask = timer::task_timer_loop(&sub_tx, &chrono::Duration::milliseconds(250));
+    let mqttsubscribetask = mqtt::task_subscription_loop(&sub_tx, eventloop);
+    let mqttpublishtask = mqtt::task_publication_loop(pub_rx, client); // or pub_tx.subscribe() if broadcast
+
+    let engine = mqtt::create_engine(Box::new(app_reducer));
+    let enginetask = engine::task_runtime_loop(pub_tx, sub_rx, engine);
+
+    let _ = try_join!(enginetask, mqttpublishtask, mqttsubscribetask, timertask)?;
+
+    log::info!("Exiting myrulesiot...");
+    Ok(())
+}
